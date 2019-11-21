f7f3ce49ab44dd1922ced38fc40f5c0c54adccca
Emil J Eriksson <eje1999@gmail.com>
2019-11-21 14:04:15 +0100

    Lagt till metoder i GameBoard; Tomma rutor hanteras som null och board består nu av Integers.

--- projektet/src/projektet/GameBoard.java
+++ projektet/src/projektet/GameBoard.java
@@ -1,6 +1,37 @@
 package projektet;
 
 public class GameBoard {
-	int board[][] = new int[9][9];
+	private Integer board[][] = new Integer[9][9]; // Måste vara Integer för att kunna sättas till null
 	
+	/*
+	 * Sets the value of the box at row and coloumn to i.
+	 */
+	public void setNumber(int i, int row, int coloumn) throws IllegalArgumentException {
+		if (i < 0 || i > 9) {
+			throw new IllegalArgumentException(i + " is not an allowed value for a box");
+		}
+		if(isLegalCoordinate(row, coloumn)) {
+		board[row][coloumn] = i;
+		}
+	}
+	
+	/*
+	 * Sets the value of (row, coloumn) to null.
+	 */
+	public void removeNumber(int row, int coloumn) {
+		if(isLegalCoordinate(row, coloumn)) {
+			board[row][coloumn] = null;
+		}
+	}
+	
+	
+	/*
+	 * Checks if (row, coloumn) is part of the board
+	 */
+	private boolean isLegalCoordinate(int row, int coloumn) throws ArrayIndexOutOfBoundsException {
+		if (row < 0 || row > 8 || coloumn < 0 || coloumn > 8) {
+			throw new ArrayIndexOutOfBoundsException("(" + row + "," + coloumn + ") is not part of the board");
+		}
+		return true;
+	}
 }
