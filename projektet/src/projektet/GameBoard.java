543178547d66e25c392722745e3b8f5f29bb25d1
Emil J Eriksson <eje1999@gmail.com>
2019-11-22 11:58:53 +0100

    Completed methods for finding regions, to be tested

--- projektet/src/projektet/GameBoard.java
+++ projektet/src/projektet/GameBoard.java
@@ -36,11 +36,50 @@ public class GameBoard {
 	
 	/* 
 	 * Returns the region of the specified coordinate as a 3x3 Integer matrix
+	 * Uses private help method getRegionFromOrigin().
 	 */
 	public Integer[][] getRegion(int row, int coloumn) {
-		Integer[][] region = new Integer[3][3];
 		if (isLegalCoordinate(row, coloumn)) {
-			
+			// Hittar var sjutton koordinaten finns. 
+			// Då vet vi var denna region har sitt övre vänstra hörn; Trycker in det i getRegionFromOrigin()
+			if (row < 3) {
+				if(coloumn < 3) {
+					return getRegionFromOrigin(0, 0);
+				} else if (coloumn < 6) {
+					return getRegionFromOrigin(0, 3);
+				} else if (coloumn < 9) {
+					return getRegionFromOrigin(0, 6);
+				}
+			} else if(row < 6) {
+				if(coloumn < 3) {
+					return getRegionFromOrigin(3, 0);
+				} else if (coloumn < 6) {
+					return getRegionFromOrigin(3, 3);
+				} else if (coloumn < 9) {
+					return getRegionFromOrigin(3, 6);
+				}
+			} else if(row < 9) {
+				if(coloumn < 3) {
+					return getRegionFromOrigin(6, 0);
+				} else if (coloumn < 6) {
+					return getRegionFromOrigin(6, 3);
+				} else if (coloumn < 9) {
+					return getRegionFromOrigin(6, 6);
+				}
+			}
+		}
+		return null;
+	}
+	
+	/*
+	 * Privat help method, returns the 3x3 integer matrix with (0,0) == (row, coloumn)
+	 */
+	private Integer[][] getRegionFromOrigin(int row, int coloumn) {
+		Integer[][] region = new Integer[3][3];
+		for (int r = row; r < row + 3; r++) {
+			for (int c = coloumn; c < coloumn + 3; c++) {
+				region[r][c] = getValue(r, c);
+			}
 		}
 		return region;
 	}
