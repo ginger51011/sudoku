38338d5fe4b340932d8af56ebdf46cf58c4a4712
Emil J Eriksson <eje1999@gmail.com>
2019-12-05 14:25:43 +0100

    PUSHA DÅ

--- projektet/src/projektet/SudokuApplication.java
+++ projektet/src/projektet/SudokuApplication.java
@@ -2,9 +2,11 @@ package projektet;
 
 public class SudokuApplication {
 	GameBoard gb = new GameBoard();
-	Solver s = new Solver();
 	
 	public static void main(String[] args) {
-		
+		SudokuApplication sa = new SudokuApplication();
+		sa.gb.setNumber(1, 5, 6);
+		sa.gb.setNumber(3, 2, 2);
+		sa.gb.setNumber(2, 1, 1);
 	}
 }
