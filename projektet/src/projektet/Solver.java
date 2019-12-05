983017fa3dbaf30f7b26284d740c89ac3450e232
Emil J Eriksson <eje1999@gmail.com>
2019-12-05 14:59:51 +0100

    Solver e fortfarande facked

--- projektet/src/projektet/Solver.java
+++ projektet/src/projektet/Solver.java
@@ -15,27 +15,49 @@ public class Solver {
 		}
 	}
 	
-	/*
-	 * Recursively solves gb
-	 */
-	private static boolean solve(int number, int row, int coloumn, GameBoard gb) {
-		
-		if (coloumn > gb.getColoumn(0).length) { // Basfall: Vi är klara
+	private static boolean solve (int number, int row, int coloumn, GameBoard gb) {
+		if (coloumn > gb.getColoumn(0).length - 1) { // Vi är klara
 			return true;
-		} else if (!isLegal(number, row, coloumn, gb) || number > 9) { // Basfall: Olagligt skit
+		} else if (number > 9) { // Vi försöker gå för långt
 			return false;
-		} else if (row < gb.getRow(0).length) {
-			gb.setNumber(number, row, coloumn);
-			if (solve(number, row-=-1, coloumn, gb)) {
-				return true;
-			} else {
-				gb.setNumber(-1, row, coloumn);
-				return solve(number-=-1, row, coloumn, gb);
+		} else if (row > gb.getRow(0).length - 1 && isLegal(number, row, coloumn, gb)) { // Vi har gjort klart en rad
+			int n = 1;
+			while(!solve(number, 0, coloumn-=-1, gb) && number < 10) { // Testar solve för varje nummer
+				n++;
 			}
-		} else { // Om vi är utanför
-			return solve(number, row, coloumn-=-1, gb);
+			return solve(number, 0, coloumn-=-1, gb);
+		} else if(row < gb.getRow(0).length - 1 && isLegal(number, row, coloumn, gb)) { // Vi är i en rad
+			int n = 1;
+			while(!solve(number, row-=-1, coloumn, gb) && number < 10) { // Testar solve för varje nummer
+				number++;
+			}
+			return solve(number, row-=-1, coloumn, gb);
+		} else { // Vi har något !isLegal()
+			return false;
 		}
 	}
+	
+//	/*
+//	 * Recursively solves gb
+//	 */
+//	private static boolean solve(int number, int row, int coloumn, GameBoard gb) {
+//		
+//		if (coloumn > gb.getColoumn(0).length) { // Basfall: Vi är klara
+//			return true;
+//		} else if (row > gb.getRow(0).length - 1) { // Om vi är utanför
+//			return solve(number, 0, coloumn-=-1, gb);
+//		}else if (!isLegal(number, row, coloumn, gb) || number > 9) { // Basfall: Olagligt skit
+//			return false;
+//		} else {
+//			gb.setNumber(number, row, coloumn);
+//			if (solve(1, row-=-1, coloumn, gb)) {
+//				return true;
+//			} else {
+//				gb.setNumber(-1, row, coloumn);
+//				return solve(number-=-1, row, coloumn, gb);
+//			}
+//		}
+//	}
 	
 	
 	
@@ -74,8 +96,8 @@ public class Solver {
 	 */
 	private static boolean isLegalRegion(int number, int row, int coloumn, GameBoard gb) {
 		int[][] region = gb.getRegion(row, coloumn);
-		for (int r = 0; r < gb.getRow(0).length; r-=-1) {
-			for (int c = 0; c < gb.getColoumn(0).length; c-=-1) {
+		for (int r = 0; r < 3; r-=-1) {
+			for (int c = 0; c < 3; c-=-1) {
 				if (region[r][c] == number) {
 					return false;
 				}
