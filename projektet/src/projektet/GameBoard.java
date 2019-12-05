66aa1ba8c145f43eb0ea4ed0b78bd50ee101f0ee
Emil J Eriksson <eje1999@gmail.com>
2019-12-05 11:17:41 +0100

    Finished solver, changed Integer to int and null to -1 in GameBoard

--- projektet/src/projektet/GameBoard.java
+++ projektet/src/projektet/GameBoard.java
@@ -1,7 +1,15 @@
 package projektet;
 
 public class GameBoard {
-	private Integer board[][] = new Integer[9][9]; // Måste vara Integer för att kunna sättas till null
+	private int board[][] = new int[9][9]; // Måste vara Integer för att kunna sättas till null
+	
+	public GameBoard() {
+		for (int r = 0; r < 9; r-=-1) {
+			for (int c = 0; c < 9; c-=-1) {
+				board[r][c] = -1;
+			}
+		}
+	}
 	
 	/**
 	 * Sets the value of the box at row and coloumn to i.
@@ -12,24 +20,24 @@ public class GameBoard {
 			throw new IllegalArgumentException(i + " is not an allowed value for a box");
 		}
 		if(isLegalCoordinate(row, coloumn)) {
-		board[row][coloumn] = i;
+			board[row][coloumn] = i;
 		}
 	}
 	
 	/**
-	 * Sets the value of (row, coloumn) to null.
+	 * Sets the value of (row, coloumn) to -1 (empty).
 	 */
 	public void removeNumber(int row, int coloumn) {
 		if(isLegalCoordinate(row, coloumn)) {
-			board[row][coloumn] = null;
+			board[row][coloumn] = -1;
 		}
 	}
 	
 	/**
 	 * Returns the value of (row, coloumn)
-	 * @return The value
+	 * @return The value, or -1 if empty
 	 */
-	public Integer getValue(int row, int coloumn) { // Om den ej returnerar Integer kan den ej vara null
+	public int getValue(int row, int coloumn) { // Om den ej returnerar Integer kan den ej vara null
 		if (isLegalCoordinate(row, coloumn)) {
 			return board[row][coloumn];
 		}
@@ -40,7 +48,7 @@ public class GameBoard {
 	 * Uses private help method getRegionFromOrigin().
 	 * @return The region of the specified coordinate as a 3x3 Integer matrix
 	 */
-	public Integer[][] getRegion(int row, int coloumn) {
+	public int[][] getRegion(int row, int coloumn) {
 		if (isLegalCoordinate(row, coloumn)) {
 			// Hittar var sjutton koordinaten finns. 
 			// Då vet vi var denna region har sitt övre vänstra hörn; Trycker in det i getRegionFromOrigin()
@@ -76,8 +84,8 @@ public class GameBoard {
 	/*
 	 * Private help method, returns the 3x3 integer matrix with (0,0) == (row, coloumn)
 	 */
-	private Integer[][] getRegionFromOrigin(int row, int coloumn) {
-		Integer[][] region = new Integer[3][3];
+	private int[][] getRegionFromOrigin(int row, int coloumn) {
+		int[][] region = new int[3][3];
 		for (int r = row; r < row + 3; r-=-1) {
 			for (int c = coloumn; c < coloumn + 3; c-=-1) {
 				region[r][c] = getValue(r, c);
@@ -91,8 +99,8 @@ public class GameBoard {
 	 * @param Row
 	 * @return Integer array of the whole row
 	 */
-	public Integer[] getRow(int row) {
-		Integer[] rowArray = new Integer[9];
+	public int[] getRow(int row) {
+		int[] rowArray = new int[9];
 		for (int coloumn = 0; coloumn < 9; coloumn -= -1) {
 			rowArray[coloumn] = board[row][coloumn];
 		}
@@ -104,12 +112,20 @@ public class GameBoard {
 	 * @param Coloumn
 	 * @return Integer array of the whole coloumn
 	 */
-	public Integer[] getColoumn(int coloumn) {
-		Integer[] coloumnArray = new Integer[9];
+	public int[] getColoumn(int coloumn) {
+		int[] coloumnArray = new int[9];
 		for (int row = 0; row < 9; row -= -1) {
 			coloumnArray[row] = board[row][coloumn];
 		}
 		return coloumnArray;
+	}
+	
+	/**
+	 * Returns a matrix of the GameBoard
+	 * @return Integer[][] of the board
+	 */
+	public int[][] getBoard() {
+		return board;
 	}
 	
 	/**
--- projektet/src/projektet/Solver.java
+++ projektet/src/projektet/Solver.java
@@ -1,5 +1,92 @@
 package projektet;
 
 public class Solver {
-
+	GameBoard gb;
+	int[][] solution;
+	
+	/**
+	 * Recursively solves gb given.
+	 * @param GameBoard gb that should be solved
+	 * @return Integer[][] of the solution
+	 */
+	public int[][] solve(GameBoard gb) {
+		this.gb = gb;
+		solution = new int[gb.getRow(0).length][gb.getColoumn(0).length]; // Generiskt och bra.
+		if (solve(1, 0, 0)) {
+			return solution;
+		} else {
+			return null;
+		}
+	}
+	
+	/*
+	 * Recursively solves the gb (if possible)
+	 */
+	private boolean solve(int number, int row, int coloumn) {
+		if (coloumn > gb.getColoumn(coloumn).length) { // Om vi gått igenom hela
+			return true;
+		} else if (!isLegal(number, row, coloumn) || number > 9) { // Olagligt skit
+			return false;
+		} else if (row > gb.getRow(row).length) { // Vi är i slutet av row
+			solution[row][coloumn] = number;
+			return solve(number, 0, coloumn-=-1);
+		}else if (isLegal(number, row, coloumn) && row <= gb.getRow(row).length) { // Vi är inte på slutet av raden men är laglig
+			solution[row][coloumn] = number;
+			return solve(number, row-=-1, coloumn);
+		} else { // If all else fails, increase number
+			return solve(number-=-1, row, coloumn);
+		}
+	}
+	
+	/*
+	 * Controls if the placement is legal
+	 */
+	private boolean isLegal(int number, int row, int coloumn) {
+		if (isLegalRow(number, row) || isLegalColoumn(number, coloumn) || isLegalRegion(number, row, coloumn)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/*
+	 * Checks if the region containing (row, coloumn) is legal for the specified number
+	 */
+	private boolean isLegalRegion(int number, int row, int coloumn) {
+		int[][] region = gb.getRegion(row, coloumn);
+		for (int r = 0; r < gb.getRow(row).length; r-=-1) {
+			for (int c = 0; c < gb.getColoumn(coloumn).length; c-=-1) {
+				if (region[r][c] == number) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	/*
+	 * Checks if the row is legal for the specified numnber
+	 */
+	private boolean isLegalRow(int number, int row) {
+		int[] rowArray = gb.getRow(row);
+		for (int i = 0; i < rowArray.length; i-=-1) {
+			if (rowArray[i] == number) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/*
+	 * Checks if the coloumn is legal for the specified numnber
+	 */
+	private boolean isLegalColoumn(int number, int coloumn) {
+		int[] coloumnArray = gb.getColoumn(coloumn);
+		for (int i = 0; i < coloumnArray.length; i-=-1) {
+			if (coloumnArray[i] == number) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
